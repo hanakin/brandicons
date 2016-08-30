f5d1931955f9baf917c2b50ef98bde71ad19b583
Michael Miday <midaym@gmail.com>
2016-08-30 17:36:58 +0200

    fix typos in gulpfile

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,4 @@
-var path        = require('path'),
-    fs          = require('fs'),
-    gulp        = require('gulp'),
+var gulp        = require('gulp'),
     prefix      = require('gulp-autoprefixer'),
     sass        = require('gulp-scss'),
     rename      = require('gulp-rename'),
@@ -13,20 +11,20 @@ var path        = require('path'),
 
 // Config
 var BROWSERS = [
-    'ie >= 11',
-    'edge >= 20',
-    'ff >= 40',
-    'chrome >= 35',
-    'safari >= 8',
-    'opera >= 35',
-    'ios >= 8'
-],
-SCSS = 'assets/scss/',
-CSS = 'demo/'
-MINIFY = true,
-DEV = 'icons/development/*',
-PRO = 'icons/production/',
-VIEW = 'demo/';
+        'ie >= 11',
+        'edge >= 20',
+        'ff >= 40',
+        'chrome >= 35',
+        'safari >= 8',
+        'opera >= 35',
+        'ios >= 8'
+    ],
+    SCSS = 'assets/scss/',
+    CSS = 'demo/',
+    MINIFY = true,
+    DEV = 'icons/development/*',
+    PRO = 'icons/production/',
+    VIEW = 'demo/';
 
 var render = function(layer){
     'use strict';
@@ -41,12 +39,12 @@ var render = function(layer){
         .pipe(prefix(BROWSERS));
 
     if (MINIFY) {
-       css = css
-       .pipe(csso())
-       .pipe(rename({
-           suffix: '.min',
-           extname: '.css'
-       }));
+        css = css
+        .pipe(csso())
+        .pipe(rename({
+            suffix: '.min',
+            extname: '.css'
+        }));
     }
 
     css = css
@@ -54,7 +52,7 @@ var render = function(layer){
         .pipe(gulp.dest(CSS));
 
     return css;
-}
+};
 
 gulp.task('css', function(){
     'use strict';
